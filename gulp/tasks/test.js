31c290320df3014d2f9f1f903202a939eb7743a3
Daniel Schmidt <dsc@in2media.dk>
2014-08-02 12:18:54 +0200

    Test Drone.io error handling

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -6,10 +6,15 @@ var handleErrors = require('../util/handleErrors');
 gulp.task('test', ['testify'], function(done) {
     var karma = require('karma').server;
 
-    karma.start({
+    var opts = {
         configFile: path.resolve('karma.conf.js'),
         singleRun: true
-    }, function (exitCode) {
+    };
+
+    //Test more browsers in CI env.
+    if (process.env.CI) opts.browsers = ["Chrome", "Firefox", "PhantomJS"];
+
+    karma.start(opts, function (exitCode) {
         if (exitCode != 0) {
             //Stop the process
             process.exit(exitCode);
@@ -34,7 +39,7 @@ gulp.task('test-watch', ['testify-watch'], function(done) {
 
 // Run e2e tests using protractor.
 // Make sure server task is running.
-gulp.task('protractor', ['build', 'webdriver:update'], function() {
+gulp.task('protractor', ['serve', 'webdriver:update'], function() {
     var protractor = require('gulp-protractor').protractor;
 
     return gulp.src(config.test.e2e)
